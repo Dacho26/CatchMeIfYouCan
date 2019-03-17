3a202146fa88f29a940d385e7e3e715bec59d926
Dacho26 <nastevskidavid6@gmail.com>
2019-03-17 11:25:26 +0100

    .
    .

--- script.js
+++ script.js
@@ -1,21 +1,53 @@
 let map;
 let uluru;
+let infoWindow;
+
+
+
 
 function initMap() {
     let uluru = {
         lat: 50.064,
         lng: 19.944
     };
-
-    let map = new google.maps.Map(
-        document.getElementById('map'), {
-            zoom: 14,
-            center: uluru
-        });
+    let map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            uluru
+        },
+        zoom: 14
+    });
     let marker = new google.maps.Marker({
         position: uluru,
-        map: map,
-        animation: google.maps.Animation.DROP,
-        draggable: true
+        map: map
     });
+
+
+    infoWindow = new google.maps.InfoWindow;
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            let pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            infoWindow.open(map);
+            map.setCenter(pos);
+        }, function () {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        handleLocationError(false, infoWindow, map.getCenter());
+
+    }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
 }
